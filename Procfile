63488dfc6d331ec175afea1338567f7a585ac4c2
Osif ungar <osif.e.u@gmail.com>
2025-10-25 14:20:21 +0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+﻿web: uvicorn slh_API.main:app --host 0.0.0.0 --port 8080
--- slh_API/main.py
+++ slh_API/main.py
@@ -1,1 +1,4 @@
 ﻿from shim_api import app
+
+# optional: small ping at import so we know this file actually loaded
+print(">>> SLH_API main loaded (slh_API.main:app)")
